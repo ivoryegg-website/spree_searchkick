5a904f04e6ea88cdfcb63b662d9525176018917e
Ilya <103741451+kalinin-ilya@users.noreply.github.com>
2022-04-25 11:19:07 +0300

    Move searchkick definitions from gem to model

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,38 +1,4 @@
 Spree::Product.class_eval do
-  searchkick word_start: [:name], settings: { number_of_replicas: 0 } unless respond_to?(:searchkick_index)
-
-  def self.autocomplete_fields
-    [:name]
-  end
-
-  def self.search_fields
-    [:name]
-  end
-
-  def search_data
-    json = {
-      name: name,
-      description: description,
-      active: available?,
-      created_at: created_at,
-      updated_at: updated_at,
-      price: price,
-      currency: currency,
-      conversions: orders.complete.count,
-      taxon_ids: taxon_and_ancestors.map(&:id),
-      taxon_names: taxon_and_ancestors.map(&:name)
-    }
-
-    Spree::Property.all.each do |prop|
-      json.merge!(Hash[prop.name.downcase, property(prop.name)])
-    end
-
-    Spree::Taxonomy.all.each do |taxonomy|
-      json.merge!(Hash["#{taxonomy.name.downcase}_ids", taxon_by_taxonomy(taxonomy.id).map(&:id)])
-    end
-
-    json
-  end
 
   def taxon_by_taxonomy(taxonomy_id)
     taxons.joins(:taxonomy).where(spree_taxonomies: { id: taxonomy_id })
@@ -58,12 +24,5 @@ Spree::Product.class_eval do
         where: search_where
       ).map(&:name).map(&:strip)
     end
-  end
-
-  def self.search_where
-    {
-      active: true,
-      price: { not: nil }
-    }
   end
 end
